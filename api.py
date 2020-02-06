363223dc09f3147f3c150e5c325e889fe82fc0f2
chris <christopher.shields143@gmail.com>
2020-02-05 16:51:47 -0800

    word_vect actually works, now

--- api.py
+++ api.py
@@ -1,31 +1,9 @@
 import re
 import cloudpickle
 
-fh = open('data/token_str.pkl', 'rb')
-token_str = cloudpickle.loads(fh.read())
-
 fh = open('data/word_vect.pkl', 'rb')
 word_vect = cloudpickle.loads(fh.read())
 
-
-
-# run our model on a search query,
-# return top 10 matching strains
-
-#model = joblib.load('model.pkl')
-
-#def find_match(q):
-#	toks = mine_text(q)
-#	probs = model.predict_proba([toks])[0]
-#	lbld = list(zip(model.named_steps['clf'].classes_, probs))
-#	lbld.sort(key=lambda x:x[1], reverse=True)
-#	lbls = [l[0] for l in lbld]
-#	return lbls[:10]
-
-
-
 if __name__ == '__main__':
 	egq = 'indica giggly happy'
-	toks  = token_str(egq)
-	vec = word_vect.transform([toks])
-	print(vec.todense())
+	print(word_vect.transform([egq]).todense().sum())
--- make/vect.py
+++ make/vect.py
@@ -4,16 +4,24 @@ import cloudpickle
 import os
 
 this_path = os.path.dirname(__file__)
-fn1 = os.path.join(this_path, '../data/cannabis_tokens.csv')
-fn2 = os.path.join(this_path, '../data/word_vect.pkl')
+fn_old = os.path.join(this_path, '../data/cannabis_tokens.csv')
+fn_new = os.path.join(this_path, '../data/word_vect.pkl')
+fn_tok = os.path.join(this_path, '../data/token_str.pkl')
+
+fh = open(fn_tok, 'rb')
+token_str = cloudpickle.loads(fh.read())
 
 # from make/data.py
-df = pd.read_csv(fn1)
+df = pd.read_csv(fn_old)
 
 def dummy(x): return x
-vect = TfidfVectorizer(preprocessor=dummy, tokenizer=dummy)
+vect = TfidfVectorizer(
+	preprocessor=dummy,
+	tokenizer=token_str,
+	analyzer='word'
+)
 vect.fit(df['tokens'])
 
 pkl = cloudpickle.dumps(vect)
-with open(fn2, 'wb') as fh:
+with open(fn_new, 'wb') as fh:
 	fh.write(pkl)
